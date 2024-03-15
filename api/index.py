458ef389d1feef0ee98634864e075b12684b44b7
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2024-03-15 18:49:39 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,8 +12,6 @@ from joblib import load, Parallel, delayed
 app = Flask(__name__)
 #CORS(app)
 
-# Load intents data from JSON file
-#intents = json.loads(open('intents.json').read())
 intents = {
   "intents": [
     {
